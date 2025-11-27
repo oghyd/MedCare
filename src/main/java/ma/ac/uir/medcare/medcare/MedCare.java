c59c2897c17e77cde4cfaaa04c4d476f6148d4cf
oghyd <adriouchomar044@gmail.com>
2025-11-27 15:08:51 +0100

    Bonjour Idriss test

---
+++ src/main/java/ma/ac/uir/medcare/medcare/MedCare.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ma.ac.uir.medcare.medcare;
+
+/**
+ *
+ * @author hhgyd
+ */
+public class MedCare {
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour Idriss");
+    }
+}
