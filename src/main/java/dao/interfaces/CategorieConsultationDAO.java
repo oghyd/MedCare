250f78f7afb5d225eb72b6a4d1be9020174a4805
oghyd <adriouchomar044@gmail.com>
2025-12-05 17:10:53 +0100

    Added the methods to be implemented

--- src/main/java/dao/interfaces/CategorieConsultationDAO.java
+++ src/main/java/dao/interfaces/CategorieConsultationDAO.java
@@ -4,10 +4,27 @@
  */
 package dao.interfaces;
 
+import java.sql.SQLException;
+import java.util.LinkedList;
+import model.CategorieConsultation;
+
 /**
  *
  * @author hhgyd
  */
 public interface CategorieConsultationDAO {
+    
+    // CREATE
+    boolean createCategorieConsultation(CategorieConsultation categorie) throws SQLException;
+
+    // READ
+    CategorieConsultation findById(int id) throws SQLException;
+    LinkedList<CategorieConsultation> findAll() throws SQLException;
+
+    // UPDATE
+    boolean update(CategorieConsultation categorie) throws SQLException;
+
+    // DELETE
+    boolean delete(int id) throws SQLException;
     
 }
