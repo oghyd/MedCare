962f4488d7b7dc90566658fff4102143a4d165bd
oghyd <adriouchomar044@gmail.com>
2025-12-05 17:01:28 +0100

    Added the methods to be implemented

--- src/main/java/dao/interfaces/PatientDAO.java
+++ src/main/java/dao/interfaces/PatientDAO.java
@@ -3,11 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package dao.interfaces;
-
+import java.sql.SQLException;
+import java.util.LinkedList;
+import model.Patient;
 /**
  *
  * @author hhgyd
  */
 public interface PatientDAO {
+    
+    boolean createPatient(Patient patient) throws SQLException;
+
+    // READ
+    Patient findPatientById(int id) throws SQLException;
+    LinkedList<Patient> findAllPatients() throws SQLException;
+
+    // SEARCH
+    LinkedList<Patient> searchPatientByName(String keyword) throws SQLException;
+
+    // UPDATE
+    boolean updatePatient(Patient patient) throws SQLException;
+
+    // DELETE
+    boolean deletePatient(int id) throws SQLException;
     
 }
