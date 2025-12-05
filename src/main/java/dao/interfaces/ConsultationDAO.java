58f8901d4df9bb282d21e97b2921c34518ee29e2
oghyd <adriouchomar044@gmail.com>
2025-12-05 17:10:46 +0100

    Added the methods to be implemented

--- src/main/java/dao/interfaces/ConsultationDAO.java
+++ src/main/java/dao/interfaces/ConsultationDAO.java
@@ -4,10 +4,52 @@
  */
 package dao.interfaces;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.Map;
+import model.Consultation;
+
 /**
  *
  * @author hhgyd
  */
 public interface ConsultationDAO {
+
+    // CREATE
+    boolean createConsultation(Consultation consultation) throws SQLException;
+
+    // READ
+    Consultation findConsultationById(int id) throws SQLException;
+    LinkedList<Consultation> findAllConsultations() throws SQLException;
+
+    // UPDATE
+    boolean updateConsultation(Consultation consultation) throws SQLException;
+
+    // DELETE
+    boolean deleteConsultation(int id) throws SQLException;
+    
+    // FILTERS
+    LinkedList<Consultation> findByDate(LocalDate date) throws SQLException;
+    LinkedList<Consultation> findByPatient(int patientId) throws SQLException;
+    LinkedList<Consultation> findByStatus(String status) throws SQLException;   // optional
+    
+    // AVAILABILITY
+    boolean isSlotAvailable(int medecinId, LocalDateTime dateHeure) throws SQLException;
+    
+    // PAYMENT
+    boolean markAsPaid(int consultationId, LocalDate datePaiement) throws SQLException;
+    
+    // DOCTOR VIEW
+    LinkedList<Consultation> findDailyForDoctor(int medecinId, LocalDate date) throws SQLException;
+    
+    // MONTHLY REPORTING
+    int countConsultationsForMonth(int year, int month) throws SQLException;
+
+    double totalRevenueForMonth(int year, int month) throws SQLException;
+
+    Map<LocalDate, Double> dailyRevenueForMonth(int year, int month) throws SQLException;
+
     
 }
