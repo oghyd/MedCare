b6a598e09fc08f682d3eb28d0734b572b772f446
oghyd <adriouchomar044@gmail.com>
2025-12-06 17:50:21 +0100

    Remove unused files

--- src/main/java/db/Connection.java
+++ src/main/java/db/Connection.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package db;
 
 /**
@@ -20,8 +16,8 @@ public class Connection {
 		java.sql.Connection conn = null;
 		try{
                     conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/tp7?" +
-                            "user=root&password=");
-		} 
+                            "user=root&password=root");
+		}
 		catch (SQLException e) {
 		    System.out.println("SQLException: " + e.getMessage());
 		    System.out.println("SQLState: "     + e.getSQLState());
